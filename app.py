77b7b51c0bbbd24a3333f42214452b7090b2922e
Jesus Lopez <110035930+jesusmlb@users.noreply.github.com>
2024-02-17 13:21:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ conn = st.connection('s3', type=FilesConnection)
 df = conn.read("streamlitbucketjm/games.csv", input_format="csv", ttl=600)
 df_2 = conn.read("streamlitbucketjm/data_recommendation.json", input_format="json")
 #with open(df_2, 'r') as json_file:
-#dict_dataset = json.load(df_2)
+dict_dataset = df_2
 df_col = pd.DataFrame(df_2)
 
 #df=pickle.load(open('movie_list.pkl','rb'))
